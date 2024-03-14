934fc5ba89bb95707d3bb681265b8f2786977dbf
ataavohra-tatvasoft <mohmmedataa.vohra@etatvasoft.com>
2024-03-14 16:02:57 +0530

    partial commit

--- src/db/models/index.ts
+++ src/db/models/index.ts
@@ -2,7 +2,6 @@
 export * from "./documents_2";
 export * from "./associations_2";
 export * from "./order_2";
-export * from "./profession_2";
 export * from "./request_2";
 export * from "./user_2";
 export * from "./requestor_2";
